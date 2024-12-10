8e7d47eaad19dd8cb048a76d8e74a0e6815c88b9
David Stránský <david.stransky@gendigital.com>
2024-12-10 13:19:05 +0100

    xx - add bootstrap and loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import HomeComponent from "./component/Home.tsx";
+import NotFoundComponent from "./component/NotFound.tsx";
+import AboutComponent from "./component/About.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <Router>
+            <div>
+                <nav>
+                        <a href="/">Home</a>
+                        <a href="/">Item 2</a>
+                        <a href="/">Item 3</a>
+                        <a href="/">Item 4</a>
+                        <a href="/about">About</a>
+                </nav>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                <>
+                    <Routes>
+                        <Route path="/" element={<HomeComponent/>}/>
+                        <Route path="/about" element={<AboutComponent/>}/>
+                        <Route path="*" element={<NotFoundComponent/>}/>
+                    </Routes>
+                </>
+            </div>
+        </Router>
+    )
 }
 
 export default App
---
+++ src/component/NotFound.tsx
@@ -0,0 +1,12 @@
+
+
+function NotFoundComponent()    {
+    return (
+        <div>
+            <h1>Not found</h1>
+            <p>Default not found page</p>
+        </div>
+    );
+}
+
+export default NotFoundComponent;
