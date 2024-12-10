e633a49050a16fee3dc22f306910adebd5925b23
David Stránský <david.stransky@gendigital.com>
2024-12-10 14:48:03 +0100

    xx - react bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import './App.scss'
-import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
-import HomeComponent from "./component/Home.tsx";
-import NotFoundComponent from "./component/NotFound.tsx";
-import AboutComponent from "./component/About.tsx";
+import {Route, BrowserRouter as Router, Routes, NavLink} from "react-router-dom";
+import HomeComponent from "./component/Home";
+import NotFoundComponent from "./component/NotFound";
+import AboutComponent from "./component/About";
 
 function App() {
     return (
         <Router>
             <div>
-                <nav>
-                        <a href="/">Home</a>
-                        <a href="/">Item 2</a>
-                        <a href="/">Item 3</a>
-                        <a href="/">Item 4</a>
-                        <a href="/about">About</a>
+                <nav className="nav">
+                        <NavLink className="nav-link" to="/">Home</NavLink>
+                        <NavLink className="nav-link" to="/neco1">Item 2</NavLink>
+                        <NavLink className="nav-link" to="/neco2">Item 3</NavLink>
+                        <NavLink className="nav-link" to="/neco3">Item 4</NavLink>
+                        <NavLink className="nav-link" to="/about">About</NavLink>
                 </nav>
 
                 <>
--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,9 +1,39 @@
-function HomeComponent()    {
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function HomeComponent() {
     return (
-        <div>
+
+        <Col>
             <h1>Home - Lorem Ipsum is simply dummy</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </div>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
+                into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+                release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+                software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <Row>
+                <Col>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                    scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
+                    into electronic typesetting, remaining essentially unchanged.
+                </Col>
+                <Col>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                    scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
+                    into electronic typesetting, remaining essentially unchanged.
+                </Col>
+                <Col>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                    scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
+                    into electronic typesetting, remaining essentially unchanged.
+                </Col>
+            </Row>
+
+        </Col>
     );
 }
 
